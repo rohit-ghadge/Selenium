be448c8a8fb794c74196f7fab0a6108b83cc5d20
Rohit ghadge <rohitghadge1000@gmail.com>
2022-02-14 18:40:50 +0530

    are you ok

--- main.java
+++ main.java
@@ -1,5 +1,5 @@
 class Simple{  
     public static void main(String args[]){  
-     System.out.println("Hello Rohit How are you");  
+     System.out.println("Hello Rohit How are you are you ok");  
     }  
 }
